76fc84e068aad1da82d8a98d1cbbecd845e621dc
Meli Yildiz <129263444+MelissaYildiz@users.noreply.github.com>
2023-06-12 10:50:41 +0200

    Add files via upload

---
+++ MinutesIntoSeconds/MinutesIntoSeconds/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace MinutesIntoSeconds
+{
+    using System;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Bitte geben Sie die Anzahl der Minuten ein:");
+            string input = Console.ReadLine(); // Benutzereingabe lesen
+
+            int minutes;
+            if (int.TryParse(input, out minutes))
+            {
+                int seconds = minutes * 60; // Minuten in Sekunden umrechnen
+
+                // Das Ergebnis ausgeben
+                Console.WriteLine("{0} Minuten entsprechen {1} Sekunden.", minutes, seconds);
+            }
+            else
+            {
+                Console.WriteLine("Ungültige Eingabe. Bitte geben Sie eine ganze Zahl ein.");
+            }
+
+            // Warten, bis der Benutzer eine beliebige Taste drückt, um das Programm zu beenden
+            Console.ReadKey();
+        }
+    }
+}
